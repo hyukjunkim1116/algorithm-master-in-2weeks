1d05531391fd1d9d327a0f363db435bd446bea56
hyukjunkim1116 <nmkk1234@naver.com>
2025-02-08 02:41:31 +0900

    bottomup

--- src/main/java/org/snust/topdowndp/냅색12865.java
+++ src/main/java/org/snust/topdowndp/냅색12865.java
@@ -1,4 +1,56 @@
-package org.snust.topdowndp;
+package org.snust.topdowndp; // package org.snust.topdowndp;
+
+//
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.util.StringTokenizer;
+//
+// public class 냅색12865 {
+//
+//    private static int[][] arr;
+//    private static int[][] dp;
+//    private static int n;
+//    private static int m;
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//         n = Integer.parseInt(st.nextToken());
+//         m = Integer.parseInt(st.nextToken());
+//
+//        arr = new int[n][2];
+//
+//        for (int i = 0; i < n; i++) {
+//            st = new StringTokenizer(br.readLine()," ");
+//            int a = Integer.parseInt(st.nextToken());
+//            int b = Integer.parseInt(st.nextToken());
+//            arr[i][0] = a;
+//            arr[i][1] = b;
+//        }
+//
+//        dp = new int[n][100010];
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < 100010; j++) {
+//                dp[i][j] = -1;
+//            }
+//        }
+//
+//        int ans = recur(0,0);
+//        System.out.println(ans);
+//    }
+//
+//    public static int recur(int cur, int w) {
+//        if (w > m) return Integer.MIN_VALUE;
+//        if (cur == n) return 0;
+//        if (dp[cur][w] != -1) return dp[cur][w];
+//
+//        dp[cur][w] = Math.max(recur(cur+1,w+arr[cur][0])+arr[cur][1],recur(cur+1,w));
+//
+//        return dp[cur][w];
+//    }
+// }
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,46 +59,48 @@ import java.util.StringTokenizer;
 
 public class 냅색12865 {
 
-    private static int[][] arr;
-    private static int[][] dp;
-    private static int n;
-    private static int m;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static int N;
+  private static int M;
+  private static int[][] arr;
+  private static int[][] dp;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    arr = new int[N][2];
 
-         n = Integer.parseInt(st.nextToken());
-         m = Integer.parseInt(st.nextToken());
+    for (int i = 0; i < N; i++) {
 
-        arr = new int[n][2];
+      st = new StringTokenizer(br.readLine(), " ");
+      arr[i][0] = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine()," ");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            arr[i][0] = a;
-            arr[i][1] = b;
-        }
+      arr[i][1] = Integer.parseInt(st.nextToken());
 
-        dp = new int[n][100010];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 100010; j++) {
-                dp[i][j] = -1;
-            }
-        }
-
-        int ans = recur(0,0);
-        System.out.println(ans);
     }
 
-    public static int recur(int cur, int w) {
-        if (w > m) return Integer.MIN_VALUE;
-        if (cur == n) return 0;
-        if (dp[cur][w] != -1) return dp[cur][w];
+    dp = new int[N][M+1];
 
-        dp[cur][w] = Math.max(recur(cur+1,w+arr[cur][0])+arr[cur][1],recur(cur+1,w));
-
-        return dp[cur][w];
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < M+1; j++) {
+        dp[i][j] = -1;
+      }
     }
+
+    int ans = recur(0, 0);
+    System.out.println(ans);
+  }
+
+  private static int recur(int cur, int w) {
+
+    if (w > M+1) return Integer.MIN_VALUE;
+    if (cur == N) return 0;
+    if (dp[cur][w] != -1) return dp[cur][w];
+    dp[cur][w] = recur(cur+1,w);
+    if (w+arr[cur][0] <= M){
+      dp[cur][w] = Math.max(dp[cur][w],recur(cur+1,w+arr[cur][0])+arr[cur][1]);
+    }
+    return dp[cur][w];
+  }
 }
--- src/main/java/org/snust/topdowndp/상담14501.java
+++ src/main/java/org/snust/topdowndp/상담14501.java
@@ -1,5 +1,49 @@
 package org.snust.topdowndp;
 
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.StringTokenizer;
+//
+//public class 상담14501 {
+//
+//  private static int N;
+//  private static int[] dp;
+//  private static int[][] table;
+//
+//  public static void main(String[] args) throws IOException {
+//    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//    N = Integer.parseInt(br.readLine());
+//
+//    table = new int[N + 1][2];
+//
+//    StringTokenizer st;
+//
+//    for (int i = 1; i < N; i++) {
+//      st = new StringTokenizer(br.readLine());
+//      int a = Integer.parseInt(st.nextToken());
+//      int b = Integer.parseInt(st.nextToken());
+//      table[i + 1][0] = a;
+//      table[i + 1][1] = b;
+//    }
+//    dp = new int[N + 1];
+//    for (int i = 0; i < N + 1; i++) {
+//      dp[i] = -1;
+//    }
+//
+//    int ans = sol(1);
+//    System.out.println(ans);
+//  }
+//
+//  public static int sol(int idx) {
+//    if (idx > N + 1) return Integer.MIN_VALUE;
+//    if (idx == N + 1) return 0;
+//    if (dp[idx] != -1) return dp[idx];
+//
+//    dp[idx] = Math.max(sol(idx + 1), sol(idx + table[idx][0]) + table[idx][1]);
+//    return dp[idx];
+//  }
+//}
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -18,8 +62,7 @@ public class 상담14501 {
     table = new int[N + 1][2];
 
     StringTokenizer st;
-
-    for (int i = 1; i < N; i++) {
+        for (int i = 1; i < N; i++) {
       st = new StringTokenizer(br.readLine());
       int a = Integer.parseInt(st.nextToken());
       int b = Integer.parseInt(st.nextToken());
@@ -35,12 +78,19 @@ public class 상담14501 {
     System.out.println(ans);
   }
 
-  public static int sol(int idx) {
-    if (idx > N + 1) return Integer.MIN_VALUE;
-    if (idx == N + 1) return 0;
-    if (dp[idx] != -1) return dp[idx];
+  private static int sol(int idx){
+    //    if (idx > N + 1) return Integer.MIN_VALUE;
+    //    if (idx == N + 1) return 0;
+    //    if (dp[idx] != -1) return dp[idx];
+    //
+    //    dp[idx] = Math.max(sol(idx + 1), sol(idx + table[idx][0]) + table[idx][1]);
+    //    return dp[idx];
 
-    dp[idx] = Math.max(sol(idx + 1), sol(idx + table[idx][0]) + table[idx][1]);
+    if (idx > N + 1) return Integer.MIN_VALUE;
+    if (idx == N+1) return 0;
+    if (dp[idx] != -1) return dp[idx];
+    dp[idx] = Math.max(sol(idx+1), sol(idx+table[idx][0])+table[idx][1]);
     return dp[idx];
   }
 }
+
